d6ebe05fa8413746c75927e92349c3023a43f95d
ayybeeshafi <abdullah.akbar.shafi@gmail.com>
2018-09-05 13:33:44 +0200

    Updated the Swagger file with the current implementation of API server. refs #8

--- src/main/scala/de/upb/cs/swt/delphi/instanceregistry/Configuration.scala
+++ src/main/scala/de/upb/cs/swt/delphi/instanceregistry/Configuration.scala
@@ -3,5 +3,4 @@ package de.upb.cs.swt.delphi.instanceregistry
 class Configuration(  //Server configuration
                     val bindHost: String = "0.0.0.0",
                     val bindPort: Int = 8087,
-
                    ) 
