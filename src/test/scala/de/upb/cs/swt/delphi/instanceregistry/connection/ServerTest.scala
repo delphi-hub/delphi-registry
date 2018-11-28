081e287f67a1c032b994319caf8f1682a49a5cf3
ayybeeshafi <abdullah.akbar.shafi@gmail.com>
2018-11-28 15:27:44 +0100

    Implementation of Test cases from Akka Http Routes respective to error codes responses #37

--- src/test/scala/de/upb/cs/swt/delphi/instanceregistry/connection/ServerTest.scala
+++ src/test/scala/de/upb/cs/swt/delphi/instanceregistry/connection/ServerTest.scala
@@ -25,12 +25,11 @@ import de.upb.cs.swt.delphi.instanceregistry.connection.Server.routes
 import de.upb.cs.swt.delphi.instanceregistry.io.swagger.client.model.{Instance, InstanceJsonSupport, InstanceLink}
 import de.upb.cs.swt.delphi.instanceregistry.io.swagger.client.model.InstanceEnums.{ComponentType, InstanceState}
 import spray.json._
-
-import scala.concurrent.{Await, Future}
+import scala.concurrent.Future
 import scala.util.Try
 
 
-class ServerTest extends WordSpec with Matchers with  ScalatestRouteTest with InstanceJsonSupport{
+class ServerTest extends WordSpec with Matchers with  ScalatestRouteTest with InstanceJsonSupport {
 
   //JSON CONSTANTS
   private val validJsonInstance = Instance(id = None, host = "http://localhost", portNumber = 4242,
@@ -45,30 +44,18 @@ class ServerTest extends WordSpec with Matchers with  ScalatestRouteTest with In
   override def beforeAll(): Unit = {
     Future(Registry.main(Array[String]()))
     Thread.sleep(3000)
-    //def a = Future(Registry.main(Array[String]()))
-    //Await.ready(a, Duration("1 second"))
-    //Await.ready(Future(Registry.main(Array[String]())),Duration("1 second"))
+
   }
 
   "The Server" should {
     "return method not allowed while registering" in {
-      Get("/register?InstanceString=25")  ~> Route.seal(routes) ~> check {
+      Get("/register?InstanceString=25") ~> Route.seal(routes) ~> check {
         assert(status === StatusCodes.METHOD_NOT_ALLOWED)
         responseAs[String] shouldEqual "HTTP method not allowed, supported methods: POST"
       }
     }
     "Successfully Register" in {
-      //Post("/register?InstanceToRegister", requestBody.toJson).withHeaders("if Any") ~> Route.seal(routes) ~> check {
-      //  status shouldBe StatusCodes.OK
 
-
-     // Post("/register", HttpEntitywithHeaders("if Any") ~> Route.seal(routes) ~> check {
-      //  status shouldBe StatusCodes.OK
-
-      //Post("/register",
-      //Post("/register" + randomId(1)+"/") ~> Route.seal(routes) ~> check {
-        //assert(status === StatusCodes.OK)
-      // HttpEntity(ContentTypes.`application/json`,"""{InstanceToRegister:Crawler}""")
 
       Post("/register", HttpEntity(ContentTypes.`application/json`,
         validJsonInstance.stripMargin)) ~> Route.seal(routes) ~> check {
@@ -80,23 +67,19 @@ class ServerTest extends WordSpec with Matchers with  ScalatestRouteTest with In
           case x =>
             fail(s"Invalid response type $x")
         }
-       //assert(response.status === 200)
-       //contentType should ===(ContentTypes.`application/json`)
-       //responseEntity shouldBe Int
-
-     }
+      }
     }
     "not register with Invalid Input" in {
       Post("/register?InstanceString=0") ~> routes ~> check {
         assert(status === StatusCodes.INTERNAL_SERVER_ERROR)
-        responseAs[String] should === ("An internal server error occurred.")
+        responseAs[String] should ===("An internal server error occurred.")
 
       }
     }
     "successfully Deregister" in {
       Post("/deregister?Id=0") ~> routes ~> check {
         assert(status === StatusCodes.OK)
-        entityAs[String] should === ("Successfully removed instance with id 0")
+        entityAs[String] should ===("Successfully removed instance with id 0")
       }
     }
     "return validation exception: Method not allowed" in {
@@ -114,9 +97,10 @@ class ServerTest extends WordSpec with Matchers with  ScalatestRouteTest with In
     "return instance not found if instance ID doesnot exists" in {
       Post("/deregister?Id=30") ~> routes ~> check {
         assert(status === StatusCodes.NOT_FOUND)
-        responseAs[String] shouldEqual("Id 30 not known to the server")
+        responseAs[String] shouldEqual ("Id 30 not known to the server")
       }
     }
+
     "throw validation exception: Method not allowed while fetching number of instances" in {
       Post("/numberOfInstances?ComponentType=Crawler") ~> Route.seal(routes) ~> check {
         assert(status === StatusCodes.METHOD_NOT_ALLOWED)
@@ -134,56 +118,55 @@ class ServerTest extends WordSpec with Matchers with  ScalatestRouteTest with In
         //responseAs[String] shouldEqual("0")
       }
     }
-    /* Always returns 200 ok even when no instance of a component is running
+    /* Always returns 200 ok even when no instance of a component is running. Tested with number of ComponentTYpe
     "Return Instances not found if no instance of specific type is present" in {
       Get("/numberOfInstances?ComponentType=WebApi") ~> routes ~> check {
         assert(status === StatusCodes.NOT_FOUND)
-
       }
-    }*/
-
-  "return POST method not allowed while matching instance" in {
-    Post("/matchingInstance?Id=0&ComponentType=ElasticSearch") ~> Route.seal(routes) ~> check {
-      assert(status === StatusCodes.METHOD_NOT_ALLOWED)
-      responseAs[String] shouldEqual "HTTP method not allowed, supported methods: GET"
     }
-  }
+*/
+    "return POST method not allowed while matching instance" in {
+      Post("/matchingInstance?Id=0&ComponentType=ElasticSearch") ~> Route.seal(routes) ~> check {
+        assert(status === StatusCodes.METHOD_NOT_ALLOWED)
+        responseAs[String] shouldEqual "HTTP method not allowed, supported methods: GET"
+      }
+    }
     /* Throwing Error 404 even with the right commands (as specified in server.scala).
-     Tested with Elastic search as it is always running. Also with other components after running them.
+     Tested after running registry and verifying ElasticSearch is registered with Id=0. Also with other components after running them.
 
     "Return Matching instance of specific type" in {
-      Get("/matchingInstance?Id=1&ComponentType=Crawler") ~> routes ~> check {
+      Get("/matchingInstance?Id=0&ComponentType=ElasticSearch") ~> routes ~> check {
       assert(status === StatusCodes.OK)
     }
   }
 */
-  "return bad request:Could not find matching instance" in {
-    Get("/matchingInstance?Id=0&ComponentType=ElasticSarch") ~> Route.seal(routes) ~> check {
-      assert(status === StatusCodes.BAD_REQUEST)
+    "return bad request:Could not find matching instance" in {
+      Get("/matchingInstance?Id=0&ComponentType=ElasticSarch") ~> Route.seal(routes) ~> check {
+        assert(status === StatusCodes.BAD_REQUEST)
+      }
     }
-  }
-  "return POST method not allowed while fetching instance" in {
-    Post("/instances?ComponentType=ElasticSearch") ~> Route.seal(routes) ~> check {
-      assert(status === StatusCodes.METHOD_NOT_ALLOWED)
-      responseAs[String] shouldEqual "HTTP method not allowed, supported methods: GET"
+    "return POST method not allowed while fetching instance" in {
+      Post("/instances?ComponentType=ElasticSearch") ~> Route.seal(routes) ~> check {
+        assert(status === StatusCodes.METHOD_NOT_ALLOWED)
+        responseAs[String] shouldEqual "HTTP method not allowed, supported methods: GET"
+      }
     }
-  }
     "display all matching instances" in {
       Get("/instances?ComponentType=ElasticSearch") ~> routes ~> check {
         assert(status === StatusCodes.OK)
       }
     }
-  "return failed to deserialize parameter while fetching instance" in {
-    Get("/instances?ComponentType=ElastcSearch") ~> Route.seal(routes) ~> check {
-      assert(status === StatusCodes.BAD_REQUEST)
+    "return failed to deserialize parameter while fetching instance" in {
+      Get("/instances?ComponentType=ElastcSearch") ~> Route.seal(routes) ~> check {
+        assert(status === StatusCodes.BAD_REQUEST)
+      }
     }
-  }
-  "return GET method not allowed in matching result" in {
-    Get("/matchingResult?CallerId=0&MatchedInstanceId=0&MatchingSuccessful=1") ~> Route.seal(routes) ~> check {
-      assert(status === StatusCodes.METHOD_NOT_ALLOWED)
-      responseAs[String] shouldEqual "HTTP method not allowed, supported methods: POST"
+    "return GET method not allowed in matching result" in {
+      Get("/matchingResult?CallerId=0&MatchedInstanceId=0&MatchingSuccessful=1") ~> Route.seal(routes) ~> check {
+        assert(status === StatusCodes.METHOD_NOT_ALLOWED)
+        responseAs[String] shouldEqual "HTTP method not allowed, supported methods: POST"
+      }
     }
-  }
     "return matching result not found if there is no match" in {
       Post("/matchingResult?CallerId=1&MatchedInstanceId=2&MatchingSuccessful=0") ~> routes ~> check {
         assert(status === StatusCodes.NOT_FOUND)
@@ -195,16 +178,28 @@ class ServerTest extends WordSpec with Matchers with  ScalatestRouteTest with In
       }
     }
 
-  "return method POST not allowed in eventlist" in {
-    Post("/eventList?Id=0") ~> Route.seal(routes) ~> check {
-      assert(status === StatusCodes.METHOD_NOT_ALLOWED)
-      responseAs[String] shouldEqual "HTTP method not allowed, supported methods: GET"
+
+    "return method POST not allowed in eventlist" in {
+      Post("/eventList?Id=0") ~> Route.seal(routes) ~> check {
+        assert(status === StatusCodes.METHOD_NOT_ALLOWED)
+        responseAs[String] shouldEqual "HTTP method not allowed, supported methods: GET"
+      }
     }
-  }
-  " return instance ID not found in event list" in {
-    Get("/eventList?Id=45") ~> routes ~> check {
-      assert(status === StatusCodes.NOT_FOUND)
+    " return instance ID not found in event list" in {
+      Get("/eventList?Id=45") ~> routes ~> check {
+        assert(status === StatusCodes.NOT_FOUND)
+      }
     }
-  }
+    /*
+    "Matching successful" in {
+      Post("/matchingResult?CallerId=1&MatchedInstanceId=0&MatchingSuccessful=1") ~> Route.seal(routes) ~> check {
+        assert(status === StatusCodes.OK)
+      }
+    }
+    " return instance ID " in {
+      Get("/eventList?Id=1") ~> routes ~> check {
+        assert(status === StatusCodes.OK)
+      }
+    }*/
   }
 }
