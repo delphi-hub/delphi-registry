3dbfab70b569e6d41099e8aafea810229d43f7ce
Johannes Duesing <jduesing@mail.uni-paderborn.de>
2018-11-28 21:45:40 +0100

    Fixed wrong package name, added comments, started arranging tests in meaningful way, renamed test cases.

--- src/test/scala/de/upb/cs/swt/delphi/instanceregistry/connection/ServerTest.scala
+++ src/test/scala/de/upb/cs/swt/delphi/instanceregistry/connection/ServerTest.scala
@@ -14,7 +14,8 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-package de.upb.cs.swt.delphi.registry.connection
+package de.upb.cs.swt.delphi.instanceregistry.connection
+
 import akka.http.javadsl.model.StatusCodes
 import akka.http.scaladsl.Http
 import akka.http.scaladsl.Http.ServerBinding
@@ -22,7 +23,6 @@ import akka.http.scaladsl.model._
 import akka.http.scaladsl.server.Route
 import akka.http.scaladsl.testkit.ScalatestRouteTest
 import de.upb.cs.swt.delphi.instanceregistry.Registry
-import de.upb.cs.swt.delphi.instanceregistry.connection.Server
 import org.scalatest.{Matchers, WordSpec}
 import de.upb.cs.swt.delphi.instanceregistry.connection.Server.routes
 import de.upb.cs.swt.delphi.instanceregistry.io.swagger.client.model.{Instance, InstanceJsonSupport}
@@ -31,7 +31,7 @@ import spray.json._
 
 import scala.concurrent.duration.Duration
 import scala.concurrent.{Await, Future}
-import scala.util.Try
+import scala.util.{Failure, Success, Try}
 
 
 class ServerTest extends WordSpec with Matchers with  ScalatestRouteTest with InstanceJsonSupport {
@@ -50,11 +50,17 @@ class ServerTest extends WordSpec with Matchers with  ScalatestRouteTest with In
   val bindingFuture: Future[ServerBinding] = Http().bindAndHandle(Server.routes,
     Registry.configuration.bindHost, Registry.configuration.bindPort)
 
+  /**
+    * Before all tests: Initialize handler and wait for server binding to be ready.
+    */
   override def beforeAll(): Unit = {
     Registry.requestHandler.initialize()
     Await.ready(bindingFuture, Duration(3, "seconds"))
   }
 
+  /**
+    * After all tests: Unbind the server, shutdown handler and await termination of both actor systems.
+    */
   override def afterAll(): Unit = {
     bindingFuture
       .flatMap(_.unbind())
@@ -67,14 +73,8 @@ class ServerTest extends WordSpec with Matchers with  ScalatestRouteTest with In
 
   "The Server" should {
 
-    "return method not allowed while registering" in {
-      Get("/register?InstanceString=25") ~> Route.seal(routes) ~> check {
-        assert(status === StatusCodes.METHOD_NOT_ALLOWED)
-        responseAs[String] shouldEqual "HTTP method not allowed, supported methods: POST"
-      }
-    }
-
-    "Successfully register when entity is valid" in {
+    //Valid register
+    "successfully register when entity is valid" in {
       Post("/register", HttpEntity(ContentTypes.`application/json`,
         validJsonInstance.stripMargin)) ~> Route.seal(routes) ~> check {
         assert(status === StatusCodes.OK)
@@ -88,8 +88,8 @@ class ServerTest extends WordSpec with Matchers with  ScalatestRouteTest with In
       }
     }
 
+    //Invalid register
     "not register when entity is invalid" in {
-
       //No entity
       Post("/register") ~> routes ~> check {
         assert(status === StatusCodes.BAD_REQUEST)
@@ -108,56 +108,139 @@ class ServerTest extends WordSpec with Matchers with  ScalatestRouteTest with In
         responseAs[String].toLowerCase should include ("could not deserialize parameter instance")
       }
 
+      //Invalid HTTP method
+      Get("/register?InstanceString=25") ~> Route.seal(routes) ~> check {
+        assert(status === StatusCodes.METHOD_NOT_ALLOWED)
+        responseAs[String] shouldEqual "HTTP method not allowed, supported methods: POST"
+      }
+
     }
-    "successfully Deregister" in {
-      Post("/deregister?Id=0") ~> routes ~> check {
+
+    //Valid deregister
+    "successfully deregister when id is valid" in {
+      var id = -1L
+
+      Post("/register", HttpEntity(ContentTypes.`application/json`,
+        validJsonInstance.stripMargin)) ~> Route.seal(routes) ~> check {
         assert(status === StatusCodes.OK)
-        entityAs[String] should ===("Successfully removed instance with id 0")
+        responseEntity match {
+          case HttpEntity.Strict(_, data) =>
+            val responseEntityString = data.utf8String
+            assert(Try(responseEntityString.toLong).isSuccess)
+            id = responseEntityString.toLong
+          case x =>
+            fail(s"Invalid response type $x")
+        }
+      }
+
+      Post(s"/deregister?Id=$id") ~> routes ~> check {
+        assert(status === StatusCodes.OK)
+        entityAs[String].toLowerCase should include ("successfully removed instance")
       }
     }
-    "return validation exception: Method not allowed" in {
+
+    //Invalid deregister
+    "not deregister if method is invalid, id is missing or invalid" in {
+      //Id missing
+      Post("/deregister") ~> Route.seal(routes) ~> check {
+        assert(status === StatusCodes.NOT_FOUND)
+        responseAs[String].toLowerCase should include ("missing required query parameter")
+      }
+
+      //Id wrong type
+      Post("/deregister?Id=kilo") ~> Route.seal(routes) ~> check {
+        assert(status === StatusCodes.BAD_REQUEST)
+        responseAs[String].toLowerCase should include ("not a valid 64-bit signed integer value")
+      }
+
+      //Id not present
+      Post(s"/deregister?Id=${Long.MaxValue}") ~> Route.seal(routes) ~> check {
+        assert(status === StatusCodes.NOT_FOUND)
+        responseAs[String].toLowerCase should include ("not known to the server")
+      }
+
+      //Wrong HTTP method
       Get("/deregister?Id=0") ~> Route.seal(routes) ~> check {
         assert(status === StatusCodes.METHOD_NOT_ALLOWED)
         responseAs[String] shouldEqual "HTTP method not allowed, supported methods: POST"
       }
     }
-    "return could not deregister instance when wrong parameter is passed" in {
-      Post("/deregister?Id=kilo") ~> Route.seal(routes) ~> check {
+
+    //Valid get instances
+    "successfully retrieve list of instances if parameter is valid" in {
+      Get("/instances?ComponentType=ElasticSearch") ~> routes ~> check {
+        assert(status === StatusCodes.OK)
+        Try(responseAs[String].parseJson.convertTo[List[Instance]](listFormat(instanceFormat))) match {
+          case Success(listOfESInstances) =>
+            listOfESInstances.size shouldEqual 1
+            listOfESInstances.exists(instance => instance.name.equals("Default ElasticSearch Instance")) shouldBe true
+          case Failure(ex) =>
+            fail(ex)
+        }
+
+      }
+      //No instances of that type present, still need to be 200 OK
+      Get("/instances?ComponentType=WebApp") ~> routes ~> check {
+        assert(status === StatusCodes.OK)
+      }
+    }
+
+    //Invalid get instances
+    "not retrieve instances if method is invalid, ComponentType is missing or invalid" in {
+      //Wrong HTTP method
+      Post("/instances?ComponentType=Crawler") ~> Route.seal(routes) ~> check {
+        assert(status === StatusCodes.METHOD_NOT_ALLOWED)
+        responseAs[String] shouldEqual "HTTP method not allowed, supported methods: GET"
+      }
+
+      //Wrong parameter value
+      Get("/instances?ComponentType=Crawlerr") ~> routes ~> check {
         assert(status === StatusCodes.BAD_REQUEST)
-
-      }
-    }
-    "return instance not found if instance ID doesnot exists" in {
-      Post("/deregister?Id=30") ~> routes ~> check {
-        assert(status === StatusCodes.NOT_FOUND)
-        responseAs[String] shouldEqual ("Id 30 not known to the server")
+        responseAs[String].toLowerCase should include ("could not deserialize parameter")
       }
     }
 
-    "throw validation exception: Method not allowed while fetching number of instances" in {
+    //Valid get number of instances
+    "successfully retrieve number of instances if parameter is valid" in {
+      Get("/numberOfInstances?ComponentType=ElasticSearch") ~> routes ~> check {
+        assert(status === StatusCodes.OK)
+        Try(responseAs[String].toLong) match {
+          case Success(numberOfEsInstances) =>
+            numberOfEsInstances shouldEqual 1
+          case Failure(ex) =>
+            fail(ex)
+        }
+
+      }
+
+      //No instances of that type present, still need to be 200 OK
+      Get("/numberOfInstances?ComponentType=WebApp") ~> routes ~> check {
+        assert(status === StatusCodes.OK)
+        Try(responseAs[String].toLong) match {
+          case Success(numberOfEsInstances) =>
+            numberOfEsInstances shouldEqual 0
+          case Failure(ex) =>
+            fail(ex)
+        }
+      }
+    }
+
+    //Invalid get number of instances
+    "not retrieve number of instances if method is invalid, ComponentType is missing or invalid" in {
+      //Wrong HTTP method
       Post("/numberOfInstances?ComponentType=Crawler") ~> Route.seal(routes) ~> check {
         assert(status === StatusCodes.METHOD_NOT_ALLOWED)
         responseAs[String] shouldEqual "HTTP method not allowed, supported methods: GET"
       }
-    }
-    "return Error 400 Bad Request" in {
+
+      //Wrong parameter value
       Get("/numberOfInstances?ComponentType=Crawlerr") ~> routes ~> check {
         assert(status === StatusCodes.BAD_REQUEST)
+        responseAs[String].toLowerCase should include ("could not deserialize parameter")
       }
     }
-    "should display number of instances of specific Component Type" in {
-      Get("/numberOfInstances?ComponentType=ElasticSearch") ~> routes ~> check {
-        assert(status === StatusCodes.OK)
-        //responseAs[String] shouldEqual("0")
-      }
-    }
-    /* Always returns 200 ok even when no instance of a component is running. Tested with number of ComponentTYpe
-    "Return Instances not found if no instance of specific type is present" in {
-      Get("/numberOfInstances?ComponentType=WebApi") ~> routes ~> check {
-        assert(status === StatusCodes.NOT_FOUND)
-      }
-    }
-*/
+
+
     "return POST method not allowed while matching instance" in {
       Post("/matchingInstance?Id=0&ComponentType=ElasticSearch") ~> Route.seal(routes) ~> check {
         assert(status === StatusCodes.METHOD_NOT_ALLOWED)
